df04c21e7b75b8907c178f3eb0cc2935bcc33895
Titus Mumo <titusmumo@gmail.com>
2022-09-30 00:35:16 +0300

    Completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
-}
+  function BlogContent(props) {
+    console.log(props);
+  
+    if (!props.isPublished) {
+      return null;
+    } else {
+      return (
+        <div>
+          <h1>{props.articleText}</h1>
+          <p>{props.minutesToRead} minutes to read</p>
+        </div>
+      );
+    }
+  }}
 
 export default BlogContent;
